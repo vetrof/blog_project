cbd0122d3583a0b8116b6824511b6b4a901ac682
Vitaly Vetrof <vetrof@gmail.com>
2023-05-29 15:37:43 +0600

    update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from blog.models import Post
 
@@ -15,8 +16,26 @@ class BlogListView(ListView):
 #     template_name = 'post_detail.html'
 
 
-
 def BlogDetailView(request, pk):
     post_detail = Post.objects.get(pk=pk)
     contex = {'post_detail': post_detail}
     return render(request, 'post_detail.html', contex)
+
+
+class BlogCreateView(CreateView):
+    template_name = 'post_new.html'
+    fields = '__all__'
+    model = Post
+    # success_url = '/'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import BlogListView, BlogDetailView
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView, name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
